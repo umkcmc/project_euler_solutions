7c7756e7262f2650f3ea40e725f3ac45d778877f
umkcmc <103076838+umkcmc@users.noreply.github.com>
2022-04-05 16:59:42 -0500

    Create problem_1.py

---
+++ problem_1.py
@@ -0,0 +1,19 @@
+# O(n) solution
+# def solution(n=1000):
+#     return sum(i for i in range(n) if i % 3 == 0 or i % 5 == 0)
+
+# O(1) solution
+def solution(n=1000):
+    term = n - 1
+    term3 = term // 3
+    term5 = term // 5
+    term15 = term // 15
+
+    return (
+        3 * term3 * (term3 + 1)
+        + 5 * term5 * (term5 + 1)
+        - 15 * term15 * (term15 + 1)
+    ) // 2
+
+
+print(solution())
